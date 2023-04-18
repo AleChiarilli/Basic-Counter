c1ea64b2e58b505a728c03d7fd62e49096bf85e8
AleChiarilli <alejandrochiarilli@gmail.com>
2023-04-18 11:16:11 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
+import "../../styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+  return (
+    <>
+      <div className="background mx-auto">
+      <div className="card">{props.element}</div>
+        <div className="card">{props.num6}</div>
+        <div className="card">{props.num5}</div>
+        <div className="card">{props.num4}</div>
+        <div className="card">{props.num3}</div>
+        <div className="card">{props.num2}</div>
+        <div className="card">{props.num}</div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -9,4 +11,43 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let num = 0;
+let num2 = 0;
+let num3 = 0;
+let num4 = 0;
+let num5 = 0;
+let num6 = 0;
+const element = <FontAwesomeIcon icon={faClock} />
+
+setInterval(function () {
+  num++;
+  if (num > 9) {
+    num = 0;
+    num2++;
+    if (num2 > 9) {
+      num2 = 0;
+      num3++;
+      if (num3 > 9) {
+        num3 = 0;
+        num4++;
+        if (num4 > 9) {
+          num4 = 0;
+          num5++;
+          if (num5 > 9) {
+            num5 = 0;
+            num6++;
+            if (num6 > 9) {
+                num6 = 0;
+            }            
+          }
+        }
+      }
+    }
+  }
+  ReactDOM.render(
+    <Home num={num} num2={num2} num3={num3} num4={num4} num5={num5} num6={num6} element={element}/>,
+    document.querySelector("#app")
+  );
+}, 1000);
+
+
